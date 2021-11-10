9b6ff274c8b237f8427f47c66584268243281b2a
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-11-09 21:12:48 -0700

    Seed file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :sessions
+  has_many :timelapses
 
   validates :name, presence: true, uniqueness: true
   validates :client, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'creating seed'
+
+4.times do
+  new_user = User.create!(
+    email: Faker::Internet.email,
+    password: '123456'
+  )
+  10.times do
+    new_project = Project.create!(
+      name: Faker::TvShows::SiliconValley.app,
+      client: Faker::TvShows::SiliconValley.company,
+      user_id: new_user.id
+    )
+    3.times do
+      Timelapse.create!(
+
+        start_time: DateTime.now + (rand * 1),
+        end_time: start_time + (rand * 1),
+        description:
+      )
+    end
+  end
+
+end
+
+puts 'done'
