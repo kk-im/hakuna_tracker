2a7231295d1adb09f26d624e9fd41238df7e9810
kk-im <kellykim0996@gmail.com>
2021-11-16 21:38:48 -0600

    new changes to routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @project = Project.new
-    @projects = Project.where("user_id = ? AND completed = ? AND deadline > ?", current_user.id, false, (Date.today + 7))
-    @projects_completed = Project.where(user: current_user, completed: true).order(updated_at: :desc).limit(5)
+    if user_signed_in?
+      @project = Project.new
+      @projects = Project.where("user_id = ? AND completed = ? AND deadline > ?", current_user.id, false, (Date.today + 7))
+      @projects_completed = Project.where(user: current_user, completed: true).order(updated_at: :desc).limit(5)
+    end
+  end
+
+  def all_projects
+    if user_signed_in?
+      @project = Project.new
+      @projects = Project.all
+    end
   end
 end
