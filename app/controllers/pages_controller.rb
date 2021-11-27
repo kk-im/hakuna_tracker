69b046c218867fa958794665ded37debc70ff25c
Francisco Aguilar <fiaguilars@gmail.com>
2021-11-26 20:00:40 -0500

    code revision

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,7 +47,7 @@ class PagesController < ApplicationController
         current_user.id, params[:query], params[:query]
       )
     else
-      @projects = current_user.projects.where(completed: false)
+      @projects = current_user.projects
     end
   end
 
